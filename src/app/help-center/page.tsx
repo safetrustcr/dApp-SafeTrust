ef4ca6adc1419736eaf0d9ba08f0cfde65b47afd
Joel Vargas <vermudo.com@gmail.com>
2025-01-22 14:11:20 -0600

    feat: add Help Center page component

---
+++ src/app/help-center/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import HelpCenter from "@/components/modules/helpCenter/HelpCenter";
+
+const HelpCenterPage = () => {
+  return <HelpCenter />;
+};
+
+export default HelpCenterPage;
