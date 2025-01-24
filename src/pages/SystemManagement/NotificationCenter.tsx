9703e396e562e550ee9707f97d6a973318ef1a98
ekumamatthew <ekumamatthew1@gmail.com>
2025-01-24 22:52:44 +0100

    feat/notification center

--- src/pages/SystemManagement/NotificationCenter.tsx
+++ src/pages/SystemManagement/NotificationCenter.tsx
@@ -1,25 +1,20 @@
 import {
-  AlertCircle,
+  NotificationAnalyticsCard,
+  NotificationCard,
+} from "@/components/ui/notification-card";
+import {
   AlertTriangle,
   Bell,
   Check,
   CircleCheck,
   FileText,
   Filter,
-  XCircle,
 } from "lucide-react";
-import React, { JSX } from "react";
-
-interface CardLabel {
-  title: string;
-  subtitle: string;
-  icon: JSX.Element;
-}
+import { JSX, useState } from "react";
 
 function NotificationCenter() {
-  const [activeTab, setActiveTab] = React.useState("All"); // Track the active tab
+  const [activeTab, setActiveTab] = useState("All");
 
-  // Function to handle tab click
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
@@ -27,49 +22,51 @@ function NotificationCenter() {
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex flex-row justify-between items-start">
         <div className="flex flex-col items-start space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
+          <h2 className="text-3xl text-black dark:text-white font-bold tracking-tight">
             Notification Center
           </h2>
-          <h4 className="text-base font-normal tracking-tight">
+          <h4 className="text-base text-black dark:text-gray-200 font-normal tracking-tight">
             Manage and track all your contract notifications
           </h4>
         </div>
         <div className="w-auto items-end lg:items-center gap-3 lg:gap-8 flex flex-col lg:flex-row ">
           <button className="border-[1px] flex gap-2 lg:gap-4 rounded-md  items-center justify-center whitespace-nowrap p-2 lg:p-3 ">
             <Filter color="black" size={16} />
-            <span className="text-base text-black">Filter</span>
+            <span className="text-base text-black dark:text-gray-100">
+              Filter
+            </span>
           </button>
 
           <button className="bg-transparent/15 gap-2 lg:gap-4 items-center flex flex-row p-2 lg:p-3 whitespace-nowrap rounded-md">
             <Check size={20} color="black" className="text-black" />
-            <span className="text-base text-black "> Mark As Read</span>
+            <span className="text-base text-black  dark:text-gray-100">
+              {" "}
+              Mark As Read
+            </span>
           </button>
         </div>
       </div>
 
       <div className="w-full h-auto items-start background-transparent">
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {Object.entries(metricsData).map(([key, { total, ...extra }]) => (
-            <div key={key} className="rounded-xl border bg-card p-6">
-              <div className="flex flex-row justify-between items-center w-full">
-                <p className="text-sm font-semibold text-black">
-                  {cardLabels[key]?.title}
-                </p>
-                {cardLabels[key]?.icon}
-              </div>
-              <div>
-                <h2 className="text-2xl font-bold tracking-tight">{total}</h2>
-                <h4 className="text-xs font-normal text-muted-foreground tracking-tight">
-                  {Object.values(extra)[0]} {cardLabels[key]?.subtitle}
-                </h4>
-              </div>
-            </div>
-          ))}
+          {Object.entries(metricsData).map(([key, { total, unread }]) => {
+            const { title, subtitle, icon } = cardLabels[key];
+            return (
+              <NotificationAnalyticsCard
+                key={key}
+                total={total}
+                unread={unread}
+                title={title}
+                subtitle={subtitle}
+                icon={icon}
+              />
+            );
+          })}
         </div>
       </div>
 
       <div className="rounded-xl border bg-card p-6">
-        <h2 className="text-3xl font-bold tracking-tight">
+        <h2 className="text-3xl font-bold text-black dark:text-white tracking-tight">
           Recent Notification
         </h2>
         <div className="w-full flex-col space-y-4">
@@ -77,7 +74,9 @@ function NotificationCenter() {
             <p
               onClick={() => handleTabClick("All")}
               className={`cursor-pointer text-sm font-semibold p-1 rounded-lg ${
-                activeTab === "All" ? "bg-gray-500 text-white" : "text-gray-500"
+                activeTab === "All"
+                  ? "bg-gray-500 dark:bg-gray-200 text-white dark:text-gray-800"
+                  : "text-gray-500 dark:text-white"
               }`}
             >
               All
@@ -87,8 +86,8 @@ function NotificationCenter() {
               onClick={() => handleTabClick("Unread")}
               className={`cursor-pointer text-sm font-semibold p-1 rounded-lg ${
                 activeTab === "Unread"
-                  ? "bg-gray-500 text-white"
-                  : "text-gray-500"
+                  ? "bg-gray-500 dark:bg-gray-200 text-white dark:text-gray-800"
+                  : "text-gray-500 dark:text-white"
               }`}
             >
               Unread
@@ -98,46 +97,15 @@ function NotificationCenter() {
               onClick={() => handleTabClick("Important")}
               className={`cursor-pointer text-sm font-semibold p-1 rounded-lg ${
                 activeTab === "Important"
-                  ? "bg-gray-500 text-white"
-                  : "text-gray-500"
+                  ? "bg-gray-500 dark:bg-gray-200 text-white dark:text-gray-800"
+                  : "text-gray-500 dark:text-white"
               }`}
             >
               Important
             </p>
           </div>
-          {notifications.map(({ id, type, title, message, time, priority }) => (
-            <div
-              key={id}
-              className="flex flex-col lg:flex-row p-6 lg:gap-0 gap-3 justify-between border-gray-500 rounded-xl border  bg-sidebar items-start w-full"
-            >
-              <div className="flex flex-row gap-4">
-                <div>
-                  {type === "success" && (
-                    <CircleCheck size={20} color="green" />
-                  )}
-                  {type === "warning" && (
-                    <AlertCircle size={20} color="yellow" />
-                  )}
-                  {type === "error" && <XCircle size={20} color="red" />}
-                  {type === "info" && <Bell size={20} color="blue" />}
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <p className="text-sm font-semibold text-black">{title}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    {message}
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex flex-row justify-end gap-2 items-center">
-                {priority && (
-                  <p className="bg-red-500 text-center   rounded-full p-1 text-xs font-semibold">
-                    {priority}
-                  </p>
-                )}
-                <p className="text-xs font-semibold text-black">{time}</p>
-              </div>
-            </div>
+          {notifications.map((notification) => (
+            <NotificationCard key={notification.id} {...notification} />
           ))}
         </div>
       </div>
@@ -147,26 +115,18 @@ function NotificationCenter() {
 
 export default NotificationCenter;
 
-// Metrics data
 const metricsData = {
-  allNotifications: {
-    total: 28,
-    unread: 12,
-  },
-  contractUpdates: {
-    total: 15,
-    requireAction: 5,
-  },
-  depositEvents: {
-    total: 8,
-    pending: 3,
-  },
-  disputes: {
-    total: 5,
-    critical: 2,
-  },
+  allNotifications: { total: 28, unread: 12 },
+  contractUpdates: { total: 15, unread: 5 },
+  depositEvents: { total: 8, unread: 3 },
+  disputes: { total: 5, unread: 2 },
 };
 
+type CardLabel = {
+  title: string;
+  subtitle: string;
+  icon: JSX.Element;
+};
 const cardLabels: Record<string, CardLabel> = {
   allNotifications: {
     title: "All Notifications",
@@ -190,8 +150,16 @@ const cardLabels: Record<string, CardLabel> = {
   },
 };
 
-// Notifications list
-const notifications = [
+type NotificationType = "success" | "warning" | "error" | "info";
+
+const notifications: {
+  id: number;
+  type: NotificationType;
+  title: string;
+  message: string;
+  time: string;
+  priority?: string;
+}[] = [
   {
     id: 1,
     type: "success",
