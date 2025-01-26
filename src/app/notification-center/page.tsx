ee623337ac3156f1f9bc949e345cebd3effe6f25
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-01-25 22:20:33 -0600

    feat: update page

--- src/app/notification-center/page.tsx
+++ src/app/notification-center/page.tsx
@@ -1,9 +1,29 @@
 "use client";
+
+import { AdminSidebar } from "@/layouts/Sidebar";
+import { DashboardHeader } from "@/layouts/Header";
+import { DashboardFooter } from "@/layouts/Footer";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
 import NotificationCenter from "@/pages/SystemManagement/NotificationCenter";
-// type Props = {};
 
-function page() {
-  return <NotificationCenter />;
+export default function NotificationCenterPage() {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SidebarProvider>
+        <div className="flex min-h-screen flex-col">
+          <div className="flex flex-1">
+            <AdminSidebar />
+            <SidebarInset className="flex flex-col w-full">
+              <DashboardHeader />
+              <main className="flex-1 p-8 pt-6 space-y-4">
+                <NotificationCenter />
+              </main>
+              <DashboardFooter />
+            </SidebarInset>
+          </div>
+        </div>
+      </SidebarProvider>
+    </ThemeProvider>
+  );
 }
-
-export default page;
