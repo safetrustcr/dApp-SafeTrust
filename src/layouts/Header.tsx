5cd32171f98b53df12b0dfda763e1251b2b74675
Joel Vargas <vermudo.com@gmail.com>
2025-01-27 14:11:28 -0600

    feat: add disconnect button in DashboardHeader for wallet disconnection

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useWallet } from "@/components/auth/hooks/useWallet.hook";
+import { useGlobalAuthenticationStore } from "@/components/auth/store/data";
 import { Button } from "@/components/ui/button";
 import { Bell, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export function DashboardHeader() {
   const { theme, setTheme } = useTheme();
+  const address = useGlobalAuthenticationStore((state) => state.address);
+    const { handleDisconnect } = useWallet();
 
   return (
     <header className="flex h-16 items-center justify-between border-b px-4 lg:px-6">
@@ -24,6 +28,12 @@ export function DashboardHeader() {
           <Bell className="h-5 w-5" />
           <span className="sr-only">Notifications</span>
         </Button>
+
+        {address && (
+          <Button onClick={handleDisconnect} type="button"  className="w-full text-white bg-gradient-to-br from-blue-600 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2">
+            Disconnect
+          </Button>
+        )}
       </div>
     </header>
   );
