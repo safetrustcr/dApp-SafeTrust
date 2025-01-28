71466a6995ce8809934a68fa636ae0e773bcd16c
Daniel Coto Jimenez <dannyc0902@gmail.com>
2025-01-27 18:07:49 -0600

    feat: update header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,29 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Bell, Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import { useIsMobile } from "@/hooks/use-mobile";
+import ThemeToggle from "./ThemeToggle";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
 
 export function DashboardHeader() {
-  const { theme, setTheme } = useTheme();
+  const isMobile = useIsMobile();
 
   return (
-    <header className="flex h-16 items-center justify-between border-b px-4 lg:px-6">
-      <div className="flex items-center gap-4" />
-      <div className="flex items-center gap-4">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-        <Button variant="ghost" size="icon">
-          <Bell className="h-5 w-5" />
-          <span className="sr-only">Notifications</span>
-        </Button>
+    <header className="flex h-16 justify-between items-center border-b px-4 lg:px-6">
+      <SidebarTrigger
+        className={cn("h-10 w-10 p-3  z-0", isMobile ? " left-0" : "relative")}
+      />
+      <div className="ml-auto p-3">
+        <ThemeToggle />
       </div>
     </header>
   );
