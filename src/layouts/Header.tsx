961eb767433fc8421a0233e1ca704c3c50b5c260
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-01-27 23:08:40 -0600

    refactor: remove margin from Disconnect button in DashboardHeader

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -30,7 +30,7 @@ export function DashboardHeader() {
           <Button
             onClick={handleDisconnect}
             type="button"
-            className="w-full text-white bg-gradient-to-br from-blue-600 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2"
+            className="w-full text-white bg-gradient-to-br from-blue-600 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Disconnect
           </Button>
