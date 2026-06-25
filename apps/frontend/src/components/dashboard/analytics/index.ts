b9556798ea03410419adc296955c528d0032346a
mariocodecr <arayaroyo686@gmail.com>
2026-06-25 00:25:50 -0600

    feat(analytics): add analytics barrel export

---
+++ apps/frontend/src/components/dashboard/analytics/index.ts
@@ -0,0 +1,4 @@
+export { AnalyticsDashboard } from "./AnalyticsDashboard";
+export { ChartContainer } from "./ChartContainer";
+export { DateRangePicker } from "./DateRangePicker";
+export { MetricCard } from "./MetricCard";
